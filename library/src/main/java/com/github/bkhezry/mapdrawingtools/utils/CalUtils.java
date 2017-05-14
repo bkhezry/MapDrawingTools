d9c53355a3a1fc3fc1d96b2571e6de9a9c6c0776
Behrouz Khezry <bkhezry@gmail.com>
2017-05-14 14:56:26 +0430

    add  google_map_utils library. using in calculate Area & Length.

---
+++ library/src/main/java/com/github/bkhezry/mapdrawingtools/utils/CalUtils.java
@@ -0,0 +1,17 @@
+package com.github.bkhezry.mapdrawingtools.utils;
+
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.List;
+
+public class CalUtils {
+    public static double getArea(List<LatLng> latLngs) {
+        return SphericalUtil.computeArea(latLngs);
+    }
+
+    public static double getLength(List<LatLng> latLngs) {
+        return SphericalUtil.computeLength(latLngs);
+    }
+}
